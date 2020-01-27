1b29fba5addb251ea0a374b2dc24157ff71be0ff
Bo <wen.yu@uoit.net>
2020-01-27 18:20:34 -0500

    Update number One
    -basic score system
    -scores is now based on a 3 point system, the closest it 3 point.
    -basic start screen
    -resets the game when player miss

---
+++ ShapeShift/Assets/Script/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    //public
+    public Player player;
+    public Text scoreDisplay;
+    public GameObject startbutton;
+
+
+    //private
+    private float currentScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentScore = Mathf.Round(player.getScore());
+        scoreDisplay.text = currentScore.ToString();
+
+        if (!player.gameStarted)
+        {
+            startbutton.SetActive(true);
+        }
+    }
+
+
+    public void startGame()
+    {
+        player.gameStarted = true;
+        startbutton.SetActive(false);
+    }
+}
--- ShapeShift/Assets/Script/Player.cs
+++ ShapeShift/Assets/Script/Player.cs
@@ -12,52 +12,86 @@ public class Player : MonoBehaviour
     public Target target;
     //rotation speed
     public int speed;
+    public bool gameStarted;
 
     private SpriteRenderer sr;
     private int spriteCounter;
     private float totalScore;
+    private int acc; // 0 = miss, 1 = Good, 2, Great, 3 = Excellent
 
     // Start is called before the first frame update
     void Start()
     {
         sr = gameObject.GetComponent<SpriteRenderer>();
-        spriteCycling();
-        spriteCounter = 0;
-        totalScore = 0;
-        speed = 100;
+        gameStarted = false;
+        resetGame();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(pivotPoint.position, Vector3.forward, speed * Time.deltaTime);
-        if (Input.GetKeyDown(KeyCode.A))
+        if (gameStarted)
         {
-            spriteCycling();
-        }
-
-        //get distance to the target
-        float dist = Vector3.Distance(target.transform.position, transform.position);
-        //Debug.Log("Distance to other: " + dist);
-
-        //check if on target
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if(spriteCounter == target.getCurrentShape() && (dist <= 2.0))
+            transform.RotateAround(pivotPoint.position, Vector3.forward, speed * Time.deltaTime);
+            if (Input.GetKeyDown(KeyCode.A))
             {
-                totalScore += dist;
-                Debug.Log("Correct Shape!" +" score = " + dist);
-                Debug.Log("Total score = " + dist);
-                target.changeSprite();
-                speed *= -1;
+                spriteCycling();
             }
-            else
+
+            //get distance to the target
+            float dist = Vector3.Distance(target.transform.position, transform.position);
+            //Debug.Log("Distance to other: " + dist);
+
+            //check if on target
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                target.changeSprite();
-                Debug.Log("Miss =(");
+                if (spriteCounter == target.getCurrentShape() && (dist <= 1.0f))
+                {
+                    Debug.Log(dist);
+                    //good hit
+                    if (dist <= 1.0f && dist > 0.5f)
+                    {
+                        totalScore += 1;
+                        acc = 1;
+                    }
+                    //great hit
+                    else if (dist <= 0.5f && dist > 0.1f)
+                    {
+                        totalScore += 2;
+                        acc = 2;
+                    }
+                    //execellent hit
+                    else if (dist <= 0.1f)
+                    {
+                        totalScore += 3;
+                        acc = 3;
+                    }
+
+                    //change target to something else
+                    target.changeSprite();
+
+                    //reverse the direction
+                    speed *= -1;
+
+                    // incease the speed of the game
+                    if (speed > 0)
+                    {
+                        speed += 1;
+                    }
+                    else
+                    {
+                        speed -= 1;
+                    }
+                }
+                //player miss, reset game
+                else
+                {
+                    target.changeSprite();
+                    resetGame();
+                    gameStarted = false;
+                }
             }
         }
-
     }
 
 
@@ -75,4 +109,28 @@ public class Player : MonoBehaviour
         }
     }
 
+    void resetGame()
+    {
+        spriteCycling();
+        spriteCounter = 0;
+        totalScore = 0;
+        speed = 50;
+        acc = 0;
+    }
+
+    //score getter && setter
+    public float getScore()
+    {
+        return totalScore;
+    }
+    public void setScore()
+    {
+        totalScore = 0.0f;
+    }
+
+    //get accurcy
+    public int getAcc()
+    {
+        return acc;
+    }
 }
