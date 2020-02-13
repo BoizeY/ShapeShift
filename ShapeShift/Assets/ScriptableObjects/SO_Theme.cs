a64cc4d04a53d88c76413e62e7fd7dd844cbb504
Dan <dmaccormick@hotmail.com>
2020-02-12 22:56:53 -0500

    Created the theme application system
    The code is not the best for now but it works. Waiting on more systems before the code is further cleaned up

---
+++ ShapeShift/Assets/ScriptableObjects/SO_Theme.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Theme", menuName = "Custom/Theme", order = 1)]
+public class SO_Theme : ScriptableObject
+{
+    //--- Public Variables ---//
+    [Header("Shapes")]
+    public Sprite[] m_shapeIcons;
+    public Color m_shapeColour;
+
+    [Header("Background")]
+    public Sprite m_wheelIcon;
+    public Color m_wheelColor;
+    public Color m_backgroundColour;
+
+    [Header("Font")]
+    public Font m_fontStyle;
+    public Color m_fontScoreColour;
+    public Color m_fontHighScoreColour;
+
+    [Header("Particles")]
+    public Sprite m_successParticle;
+    public Sprite m_failParticle;
+
+    [Header("Sounds")]
+    public AudioClip m_soundHit;
+    public AudioClip m_soundMiss;
+    public AudioClip m_soundGameOver;
+}
---
+++ ShapeShift/Assets/Scripts/Theme_Manager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Theme_Manager : MonoBehaviour
+{
+    //--- Public Variables ---//
+    public SO_Theme m_selectedTheme;
+
+
+
+    //--- Unity Methods ---//
+    private void Awake()
+    {
+        // Setup the shapes
+        Player player = GameObject.FindObjectOfType<Player>();
+        player.sprites = m_selectedTheme.m_shapeIcons;
+        player.GetComponent<SpriteRenderer>().color = m_selectedTheme.m_shapeColour;
+
+        // Setup the background
+        GameObject backgroundObj = GameObject.Find("BG");
+        SpriteRenderer backgroundRend = backgroundObj.GetComponent<SpriteRenderer>();
+        backgroundRend.sprite = m_selectedTheme.m_wheelIcon;
+        backgroundRend.color = m_selectedTheme.m_wheelColor;
+        Camera.main.backgroundColor = m_selectedTheme.m_backgroundColour;
+
+        // Setup the font
+        Text[] textObjs = GameObject.FindObjectsOfType<Text>();
+        foreach (Text txt in textObjs)
+            txt.font = m_selectedTheme.m_fontStyle;
+        GameObject.Find("Score UI").GetComponent<Text>().color = m_selectedTheme.m_fontScoreColour;
+        GameObject.Find("High Score UI").GetComponent<Text>().color = m_selectedTheme.m_fontHighScoreColour;
+
+        // TODO: Setup the particles
+        // ...
+
+        // TODO: Setup the sounds
+        // ...
+    }
+}
